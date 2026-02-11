737a83ef8a2fcf47847921e9ec79676235b34e8a
Dev Chrysalis Dalal <dev@deviaze.com>
2026-02-10 19:07:56 -0600

    feat: pass ptr into sealbindings::initialize instead of using libloading

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ You should use this crate instead of binding separately to mlua or mluau.
 
 ## Usage
 
-At the top of your `seal_open_extern` function, call `sealbindings::initialize()`.
-This dynamically loads a function from the `seal` executable, exposing the entire mluau C-stack API.
+At the top of your `seal_open_extern` function, call `sealbindings::initialize(ptr)` with the
+ptr to the `LuauApi` struct passed by *seal*.
 
 After doing so, you can safely use the C-stack API methods in `sealbindings::ffi`.
--- src/lib.rs
+++ src/lib.rs
@@ -6,35 +6,18 @@ use std::sync::OnceLock;
 
 static LUAU_API: OnceLock<&'static LuauApi> = OnceLock::new();
 
-/// Calls the function `seal_get_ffi_api` from the seal executable and initializes
-/// the static LUAU_API.
+/// takes in a pointer `*const LuauApi` that should be passed to
+/// `seal_open_extern` and initialize the LUAU_API constant within this library.
 /// 
-/// After calling this function, you may call any function from `sealbindings::ffi`.
-///
+/// After calling this function, `sealbindings::ffi*` functions should be safe to call.
+/// 
 /// # Safety
-/// - Must be called exactly once, from `seal_open_extern`.
-/// - The seal executable must properly expose a function `seal_get_ffi_api`;
-///   older versions of *seal* may not have this function.
-pub unsafe fn initialize() {
-    #[cfg(unix)]
-    let lib = {
-        use libloading::os::unix::Library as UnixLib;
-        UnixLib::this()
-    };
-
-    #[cfg(windows)]
-    let lib = {
-        use libloading::os::windows::Library as WinLib;
-        WinLib::this().expect("GetModuleHandle(NULL) failed")
-    };
-
-    let func = unsafe {
-        lib.get::<unsafe extern "C-unwind" fn() -> *const LuauApi>(c"seal_get_ffi_api")
-            .expect("seal_get_ffi_api not found in seal executable; make sure you're on the latest seal!")
-    };
-
-    let ptr = unsafe {func() };
-    assert!(!ptr.is_null(), "seal_get_ffi_api returned null");
+/// - You should call this function immediately in `seal_open_extern`
+/// - Pass a valid pointer to an actual *const LuauApi
+/// - Ensure the versioning of the API provided by the version of seal you're using
+///   is supported by this library.
+pub unsafe fn initialize(ptr: *const LuauApi) {
+    assert!(!ptr.is_null(), "LuauApi pointer is null");
     unsafe {
         LUAU_API.set(&*ptr).ok();
     }
